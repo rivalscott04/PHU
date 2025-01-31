3939875733c5b5e41581911d3d01810a5d1d85e4
rivalscott04 <rival.biasrori@outlook.com>
2025-01-31 22:49:16 +0800

    perbaiki migration lagi

--- database/migrations/2025_01_24_075935_create_travel_table.php
+++ database/migrations/2025_01_24_075935_create_travel_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('travels', function (Blueprint $table) {
             $table->id();
-            $table->string('No');
             $table->string('Penyelenggara');
             $table->string('Pusat');
             $table->date('Tanggal');
