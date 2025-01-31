4d65bf410b9adf7cd9f300472f8692aaf06ce853
rivalscott04 <rival.biasrori@outlook.com>
2025-01-31 22:46:46 +0800

    perbaiki nama table lagi

--- database/migrations/2025_01_31_031512_drop_column__no_at_travel_table.php
+++ database/migrations/2025_01_31_031512_drop_column__no_at_travel_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('travel', function (Blueprint $table) {
+        Schema::table('travels', function (Blueprint $table) {
             $table->dropColumn('No');
         });
     }
@@ -21,7 +21,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('travel', function (Blueprint $table) {
+        Schema::table('travels', function (Blueprint $table) {
             $table->string('No')->nullable();
         });
     }
