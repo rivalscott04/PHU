cb7b46e65ec1c9cd081b2f74d5857a1e753fe1c4
shatahibrizi <shatahibrizi@gmail.com>
2025-02-04 19:21:41 +0800

    ubah redirect logout

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -48,6 +48,6 @@ class LoginController extends Controller
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return redirect('/login');
+        return redirect('/');
     }
 }
